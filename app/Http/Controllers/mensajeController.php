c810c1a30ac23d20624939b11aefc2b87d9b8063
estefanibojorges <estefanibojorges7@gmail.com>
2024-05-11 10:23:28 -0400

    "up"

--- app/Http/Controllers/mensajeController.php
+++ app/Http/Controllers/mensajeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Events\mensajeEvent;
-use App\Notifications\Notificaciones;
+use App\Notifications\notificaciones;
 use App\Models\Mensaje;
 use App\Models\User;
 use App\Models\Reserva;
